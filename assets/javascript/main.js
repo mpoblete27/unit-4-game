56b59280d47cbf6aceb648735fab51a317d7297c
Mickey Poblete <mickey@Setups-MacBook-Pro.local>
2018-11-19 14:56:50 -0800

    fixed math.floor to Math.floor, etc.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,7 +11,7 @@ for(var i = 0; i < crystals.length; i++){
     var crystalPic = $("<img>");
     crystalPic.attr('src', crystals[i]); //give the div item an attribute//
     $(".pic").append(crystalPic); //change the div (class=pic) to have the picture from the array//
-    $(".pic").attr("data-value", math.floor(math.random() * 10 ) + 2 ); //give the crystal a random value//
+    $(".pic").attr("data-value", Math.floor(Math.random() * 10 ) + 2 ); //give the crystal a random value//
 }
 
 //Make the Goal number the player wants to get to//
